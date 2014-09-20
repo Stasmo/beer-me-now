e051b7359245ba909ef9a7848d89f7637c1f5248
Daniel Haselhan <dhaselhan@gmail.com>
2014-09-20 12:26:55 -0700

    Adding DB Side creation of Bars and Loading of them in Admin controller

--- src/Stasmo/Example/SampleApiBundle/Controller/AdminController.php
+++ src/Stasmo/Example/SampleApiBundle/Controller/AdminController.php
@@ -20,7 +20,6 @@ class AdminController extends Controller
         $method = $request->getMethod();
         $page = $request->query->get('page');
 
-
         return $this->render(
             'StasmoExampleSampleApiBundle:Admin:index.html.twig',
             [ 'bars' => $this->getBarList($page), 'numberOfPages' => $this->getNumberOfBarPages() ]
@@ -48,6 +47,13 @@ class AdminController extends Controller
             ->add('save', 'submit', array('label' => 'Add Bar'))
             ->getForm();
 
+            $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $dm = $this->get('doctrine_mongodb')->getManager();
+            $dm->persist($bar);
+            $dm->flush();
+        }
         return $this->render(
             'StasmoExampleSampleApiBundle:Admin:add.html.twig',
             array(
@@ -58,12 +64,14 @@ class AdminController extends Controller
 
     private function getBarList($page = 0)
     {
-        $bars = [];
-        $pageSize = 10;
-        $start = $page * $pageSize;
-        for ($i = $start; $i < $start+$pageSize; $i++) {
-            $bars[] = [ 'name' => 'bar'.$i ];
-        }
+        $bars = $this->get('doctrine_mongodb')
+            ->getRepository('StasmoExampleSampleApiBundle:Bar')
+            ->findAll();
+        //$pageSize = 10;
+        //$start = $page * $pageSize;
+        //for ($i = $start; $i < $start+$pageSize; $i++) {
+        //    $bars[] = [ 'name' => 'bar'.$i ];
+        //}
 
         return $bars;
     }
